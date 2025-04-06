7cc296e5e561292c25b99c1a05788545608164e8
tmoreno <tmorenobernal@gmail.com>
2025-04-06 23:04:03 +0200

    Init project

---
+++ backoffice-service/src/main/java/com/tmoreno/mooc/backoffice/BackofficeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tmoreno.mooc.backoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackofficeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackofficeServiceApplication.class, args);
+	}
+
+}
