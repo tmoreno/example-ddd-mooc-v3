f9ddfce4bf6784474adf68afde79a8776ef4d06c
tmoreno <tmorenobernal@gmail.com>
2025-04-09 23:25:25 +0200

    Create Teacher Id

---
+++ backoffice-service/src/main/java/com/tmoreno/mooc/backoffice/teacher/domain/TeacherId.java
@@ -0,0 +1,9 @@
+package com.tmoreno.mooc.backoffice.teacher.domain;
+
+import com.tmoreno.mooc.backoffice.shared.domain.Identifier;
+
+public final class TeacherId extends Identifier {
+    public TeacherId(String value) {
+        super(value);
+    }
+}
