64e01580172f2b83e1f1bb9d995beda775da121a
tmoreno <tmorenobernal@gmail.com>
2025-04-12 00:16:30 +0200

    Implement event bus

---
+++ backoffice-service/src/main/java/com/tmoreno/mooc/backoffice/course/infrastructure/commons/SpringApplicationEventBus.java
@@ -0,0 +1,28 @@
+package com.tmoreno.mooc.backoffice.course.infrastructure.commons;
+
+import com.tmoreno.mooc.backoffice.shared.EventBus;
+import com.tmoreno.mooc.backoffice.shared.domain.events.DomainEvent;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public final class SpringApplicationEventBus implements EventBus {
+
+    private final ApplicationEventPublisher publisher;
+
+    public SpringApplicationEventBus(ApplicationEventPublisher publisher) {
+        this.publisher = publisher;
+    }
+
+    @Override
+    public void publish(DomainEvent event) {
+        publisher.publishEvent(event);
+    }
+
+    @Override
+    public void publish(List<DomainEvent> events) {
+        events.forEach(this::publish);
+    }
+}
