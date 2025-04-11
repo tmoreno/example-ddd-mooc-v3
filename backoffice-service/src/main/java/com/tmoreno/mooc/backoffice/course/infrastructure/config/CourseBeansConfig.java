1bf8128e81d7ef11ce098fc73dee72b41cdf6d97
tmoreno <tmorenobernal@gmail.com>
2025-04-12 00:16:48 +0200

    Register bean

---
+++ backoffice-service/src/main/java/com/tmoreno/mooc/backoffice/course/infrastructure/config/CourseBeansConfig.java
@@ -0,0 +1,18 @@
+package com.tmoreno.mooc.backoffice.course.infrastructure.config;
+
+import com.tmoreno.mooc.backoffice.course.application.create.CreateCourseUseCase;
+import com.tmoreno.mooc.backoffice.course.application.create.ForCreateCourse;
+import com.tmoreno.mooc.backoffice.course.domain.CourseRepository;
+import com.tmoreno.mooc.backoffice.shared.EventBus;
+import com.tmoreno.mooc.backoffice.shared.TransactionalService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CourseBeansConfig {
+
+    @Bean
+    public ForCreateCourse forCreateCourse(CourseRepository repository, TransactionalService transactionalService, EventBus eventBus) {
+        return new CreateCourseUseCase(repository, transactionalService, eventBus);
+    }
+}
